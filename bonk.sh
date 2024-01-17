8d1b322d8fd8d959aa6c0bc57ee2d62a42c480f6
lazil0021 <156865073+lazil0021@users.noreply.github.com>
2024-01-17 08:16:45 +0700

    Create bonk.sh

---
+++ bonk.sh
@@ -0,0 +1,3 @@
+#bonk
+sudo apt update
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.4/SRBMiner-Multi-2-3-4-Linux.tar.xz && tar -xf SRBMiner-Multi-2-3-4-Linux.tar.xz && cd SRBMiner-Multi-2-3-4 && chmod +x SRBMiner-MULTI && ./SRBMiner-MULTI --disable-gpu --algorithm randomx --pool stratum+ssl://rx.unmineable.com:4444 --wallet BONK:DiohtKrQpTvQRGeacbnHwUJGWMjDvdLkvUrymMfXunkz.tes --password x --cpu-threads 4
